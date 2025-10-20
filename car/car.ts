b1fe323748904c09df74a7f4b1e74cf8bb17cc66
Gidschge <94117871+Gidschge@users.noreply.github.com>
2025-10-20 10:30:38 +0200

    Weitere objekte

--- car/car.ts
+++ car/car.ts
@@ -1,12 +1,20 @@
 export class Auto {
-    // Kapselung
-    // objektvariablen, instanzvariablen
+    // Kapselung (private Variablen)
     private brand: string;
     private speed: number = 0;
+    private engine: string;
+    private color: string;
+    private fuelLevel: number;
+    private doors: number;
 
-    constructor(brand: string, speed: number) {
+    // Konstruktor
+    constructor(brand: string, speed: number, engine: string, color: string, fuelLevel: number, doors: number) {
         this.brand = brand;
         this.setAndCheckSpeed(speed);
+        this.engine = engine;
+        this.color = color;
+        this.fuelLevel = fuelLevel;
+        this.doors = doors;
     }
 
     // Getter
@@ -18,6 +26,22 @@ export class Auto {
         return this.speed;
     }
 
+    public getEngine(): string {
+        return this.engine;
+    }
+
+    public getColor(): string {
+        return this.color;
+    }
+
+    public getFuelLevel(): number {
+        return this.fuelLevel;
+    }
+
+    public getDoors(): number {
+        return this.doors;
+    }
+
     // Setter
     public setBrand(brand: string) {
         this.brand = brand;
@@ -25,6 +49,26 @@ export class Auto {
 
     public setSpeed(speed: number) {
         this.setAndCheckSpeed(speed);
+    }
+
+    public setEngine(engine: string) {
+        this.engine = engine;
+    }
+
+    public setColor(color: string) {
+        this.color = color;
+    }
+
+    public setFuelLevel(fuelLevel: number) {
+        if (fuelLevel > 100) this.fuelLevel = 100;
+        else if (fuelLevel < 0) this.fuelLevel = 0;
+        else this.fuelLevel = fuelLevel;
+    }
+
+    public setDoors(doors: number) {
+        if (doors >= 2 && doors <= 5) {
+            this.doors = doors;
+        }
     }
 
     // Private Methode zur Kontrolle
